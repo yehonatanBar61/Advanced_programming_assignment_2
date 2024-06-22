d36d26171d776a1a552a08f8d6bd6b76f38c67b1
yehonatanBar61 <ubuntu@LAPTOP-J7QNRBGH.localdomain>
2024-06-22 17:43:34 +0300

    working with out loxkes on the counter and with flag var

--- primeCounterCP.c
+++ primeCounterCP.c
@@ -2,58 +2,67 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include "queue/lfq.h"
-
+#include <unistd.h>
+ 
 #define C_NUM_THREADS 4
 #define P_NUM_THREADS 1
+
+typedef struct {
+    int tid;       // thread id
+    int counter; 
+} arg_set;
+
+
 int total_counter; // using method 2 from the book
 struct lfq_ctx queue;
 int total_dequeued_number;
 int total_enqueued_number;
+int producer_finished = 0;
 
-pthread_mutex_t dequeued_lock = PTHREAD_MUTEX_INITIALIZER; // the lockes limites the program to be fast
-pthread_mutex_t enqueued_lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t queue_lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t flag = PTHREAD_COND_INITIALIZER;
+
 
 void *producer(void *arg) {
-    int tid = *(int *)arg;
-
-
+    arg_set *set = arg;
+    int tid = set->tid;
     int num;
     while (scanf("%d", &num) != EOF) {   
         int *data = malloc(sizeof(int));
         *data = num;
+        set->counter++;
         lfq_enqueue(&queue, data);
-        pthread_mutex_lock(&enqueued_lock);
-        total_enqueued_number++;
-        pthread_mutex_unlock(&enqueued_lock);
         printf("Producer %d enqueued %d\n", tid, *data);
     }
+    producer_finished = 1;
     return NULL;
 }
 
 void *consumer(void *arg) {
-    int tid = *(int *)arg;
+    arg_set *set = arg;
+    int tid = set->tid;
+    
     int *data;
-
     while(1) {
         data = (int *)lfq_dequeue_tid(&queue, tid);
         if (data) {
-            pthread_mutex_lock(&dequeued_lock);
-            total_dequeued_number++;
-            pthread_mutex_unlock(&dequeued_lock);
+            set->counter++;
             printf("Consumer %d dequeued %d\n", tid, *data);
             free(data);
         } else {
             printf("Consumer %d found queue empty\n", tid);
-            break;
+            if(producer_finished){
+                break;
+            }
         }
+        
     }
     return NULL;
 }
 
 int main() {
     pthread_t producers[P_NUM_THREADS], consumers[C_NUM_THREADS];
-    int producer_ids[P_NUM_THREADS];
-    int consumer_ids[C_NUM_THREADS];
+    arg_set producer_set[P_NUM_THREADS], consumer_set[C_NUM_THREADS];
 
     // Initialize the LFQ
     if (lfq_init(&queue, C_NUM_THREADS) != 0) {
@@ -61,10 +70,12 @@ int main() {
         return EXIT_FAILURE;
     }
 
+    
     // Create producer threads
     for (int i = 0; i < P_NUM_THREADS; ++i) {  
-        producer_ids[i] = i;
-        if (pthread_create(&producers[i], NULL, producer, &producer_ids[i]) != 0) {
+        producer_set[i].tid = i;
+        producer_set[i].counter = 0;
+        if (pthread_create(&producers[i], NULL, producer, &producer_set[i]) != 0) {
             fprintf(stderr, "Failed to create producer thread %d\n", i);
             return EXIT_FAILURE;
         }
@@ -72,22 +83,25 @@ int main() {
 
     // Create consumer threads
     for (int i = 0; i < C_NUM_THREADS; ++i) {
-        consumer_ids[i] = i;
-        if (pthread_create(&consumers[i], NULL, consumer, &consumer_ids[i]) != 0) {
+        consumer_set[i].tid = i;
+        consumer_set[i].counter = 0;
+        if (pthread_create(&consumers[i], NULL, consumer, &consumer_set[i]) != 0) {
             fprintf(stderr, "Failed to create consumer thread %d\n", i);
             return EXIT_FAILURE;
         }
     }
 
-    for (int i = 0; i < P_NUM_THREADS; ++i) {
+    for (int i = 0; i < P_NUM_THREADS; i++) {
+        total_enqueued_number += producer_set[i].counter;
         pthread_join(producers[i], NULL);
     }
 
-    for (int i = 0; i < C_NUM_THREADS; ++i) {
+    for (int i = 0; i < C_NUM_THREADS; i++) {
+        total_dequeued_number += consumer_set[i].counter;
         pthread_join(consumers[i], NULL);
     }
 
-    printf("%d %d\n", total_dequeued_number, total_enqueued_number);
+    printf("out: %d in: %d\n", total_dequeued_number, total_enqueued_number);
 
     // Clean up the LFQ
     if (lfq_clean(&queue) != 0) {
